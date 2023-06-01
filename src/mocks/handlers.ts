05ec9e6bfaa8eabcd12fb89baecef152f8a2894f
Tirmary Reyes <tmrv1991@gmail.com>
2023-06-01 17:25:43 +0200

    Add mocks handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,7 +4,7 @@ import { tokenMock } from "./userMocks";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const handlers = [
-  rest.get(`${apiUrl}user`, (_req, res, ctx) => {
+  rest.get(`${apiUrl}user/login`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ token: tokenMock }));
   }),
 ];
