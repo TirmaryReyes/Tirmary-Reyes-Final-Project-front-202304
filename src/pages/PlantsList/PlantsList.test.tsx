184d258cd9434b8892057dbae3c53b7fb70db91f
Tirmary Reyes <tmrv1991@gmail.com>
2023-06-04 15:52:53 +0200

    Add PlantsList test with use case

---
+++ src/pages/PlantsList/PlantsList.test.tsx
@@ -0,0 +1,17 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../testUtils/testUtils";
+import PlantsList from "./PlantsList";
+
+describe("Given a PlantsList page", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a 'Welcome to our plants list!' title", () => {
+      const expectedTitle = "Welcome to our plants list!";
+
+      renderWithProviders(<PlantsList />);
+
+      const title = screen.getByRole("heading", { name: expectedTitle });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
+});
