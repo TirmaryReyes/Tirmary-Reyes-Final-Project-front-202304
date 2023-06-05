b8d5eb837d7faeb28c888ece514bad6c795825cd
Tirmary Reyes <tmrv1991@gmail.com>
2023-06-05 11:36:50 +0200

    Add plantsMockswith structure

---
+++ src/mocks/plantsMocks.ts
@@ -0,0 +1,26 @@
+import { PlantDataStructure } from "../store/plant/types";
+
+export const plantsMocks: PlantDataStructure[] = [
+  {
+    id: "647083660ca1f98975830b0b",
+    name: "Aleo Vera",
+    image: "https://example.png",
+    type: "Helianthus annuus",
+    size: "medium",
+    hasFlowers: false,
+    environment: "outdoor",
+    description:
+      "Thrives in indirect light, well-draining soil, moderate watering.",
+  },
+  {
+    id: " 647083660ca1f98975830b0b",
+    name: "Tulip",
+    image: "https://tulip_flower.jpg",
+    type: "Liliaceae",
+    size: "medium",
+    hasFlowers: false,
+    environment: "outdoor",
+    description:
+      "Thrives in indirect light, well-draining soil, and moderate watering.",
+  },
+];
