9439390d560810a9463cc3e312ac3bd091a1bdb7
Tirmary Reyes <tmrv1991@gmail.com>
2023-06-06 21:31:49 +0200

    Add getPlants test for error use case

--- src/hooks/usePlant/usePlant.test.ts
+++ src/hooks/usePlant/usePlant.test.ts
@@ -3,6 +3,8 @@ import { plantsMocks } from "../../mocks/plantsMocks";
 import usePlant from "./usePlant";
 import { wrapper } from "../../testUtils/testUtils";
 import { PlantDataStructure } from "../../store/plant/types";
+import { server } from "../../mocks/server";
+import { errorHandlers } from "../../mocks/handlers";
 
 describe("Given a getPlants function", () => {
   describe("When is called", () => {
@@ -18,6 +20,24 @@ describe("Given a getPlants function", () => {
       const plants = await getPlants();
 
       expect(plants).toStrictEqual(plantList);
+    });
+  });
+
+  describe("When it's called but can't connect to the Api Rest", () => {
+    test("Then it should throw a 'Can't get the list of plants' error", () => {
+      server.resetHandlers(...errorHandlers);
+
+      const expectedError = "Can't get the list of plants";
+
+      const {
+        result: {
+          current: { getPlants },
+        },
+      } = renderHook(() => usePlant(), { wrapper: wrapper });
+
+      const listPlant = getPlants();
+
+      expect(listPlant).rejects.toThrowError(expectedError);
     });
   });
 });
